18c775a1518039d14cd83e39e64fbce1420801d2
JoshuaRiches <71353999+JoshuaRiches@users.noreply.github.com>
2022-09-19 21:24:28 +0100

    target range

--- Research_Intro_Project/Assets/Scripts/Enemy.cs
+++ Research_Intro_Project/Assets/Scripts/Enemy.cs
@@ -9,12 +9,33 @@ public class Enemy : MonoBehaviour
     private float currentHp;
     public bool isDead;
 
-    //[Header("Movement")]
-    //public float moveSpeed;
+    [Header("Movement")]
+    public float moveSpeed;
+    public Vector3 startPos;
+    public Vector3 endPos;
+    private Vector3 targetPos;
+    public bool isMoving;
 
     private void Start()
     {
         currentHp = maxHp;
+        targetPos = endPos;
+    }
+
+    private void Update()
+    {
+        if (!isMoving) return;
+
+        if (Vector3.Magnitude(transform.position - targetPos) > 0.5f)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed * Time.deltaTime);
+        }
+
+        if (Vector3.Magnitude(transform.position - targetPos) <= 0.5f)
+        {
+            if (targetPos == startPos) targetPos = endPos;
+            else targetPos = startPos;
+        }
     }
 
     public void Damage(float damage)
